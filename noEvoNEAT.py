53906c98318b84121f033f256c776d201e9bbd91
Terry Tao <terrytao19@gmail.com>
2019-08-02 00:03:04 -0400

    Update noEvoNEAT.py

--- noEvoNEAT.py
+++ noEvoNEAT.py
@@ -1,7 +1,13 @@
+'''
+nodes 1,2,3 and 4 are input nodes
+nodes 5 and 6 are output nodes
+'''
+
 import numpy as np
 
 class Net():
 	def create(self):
+		#Manually set genes, haven't gotten to the evolution step yet
 		self.gene = [[[1],[7],[.2],[1],[1]],
     				 [[2],[8],[.7],[1],[2]],
     				 [[3],[7],[.4],[1],[3]],
@@ -10,13 +16,16 @@ class Net():
     				 [[8],[9],[.8],[1],[6]],
     				 [[9],[6],[.5],[1],[7]],
     				 [[8],[5],[1],[1],[8]]]
-
+		
+	#sigmoid as activation function
 	def sig(self, x, derivative = False):
 		sigm = 1. / (1. + np.exp(-x))
 		if derivative:
 			return(sigm * (1. - sigm))
 		return(sigm)
-
+	
+	#findio() finds all the connection genes that contain a specific connected node
+	#example: node 7 is the output node of connection gene with innovation number of 3
 	def findio(self, value, i = False, o = False):
 		allfound = []
 		if i is True:
@@ -34,9 +43,11 @@ class Net():
 
 		return(allfound)
 
-
+	#compute() calculates the final value of any node, using recursion.
+	#recursion is used in order to ensure that all the values have been calculated for the previous nodes before calculating the next.
 	def compute(self, node, observation):
 		sigWE = 0
+		#Critical to recursion, similar to recursive fibonacci algorthm, sets the "base" / input values
 		if node == 0: return(observation[0])
 		elif node == 1: return(observation[1])
 		elif node == 2: return(observation[2])
@@ -54,4 +65,7 @@ class Net():
 
 agent = Net()
 agent.create()
+			#This is the observation, or 4 inputs
 print(agent.compute(6, [-1.0,.5,20,.7]))
+		   #5 and 6 represent the output nodes, we are calculating their values
+print(agent.compute(5, [-1.0,.5,20,.7]))
